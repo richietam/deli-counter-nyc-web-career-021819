3346b8eaa3059cf3c2188aedc200387fe0bc8ae1
richardtam.ny <richardtam.ny@gmail.com>
2019-02-10 22:08:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,12 @@ katz_deli = []
 
 def line(katz_deli)
   if katz_deli.length == 0
-    puts "The line is currently empty"
+    puts "The line is currently empty."
   else katz_deli.length == 1
-    puts "Cur"
+    newArr = []
+    katz_deli.each_with_index do |item, i|
+      newArr.push("#{i +1}. #{item}.")
+    end
+    puts "The line is currently: #{newArr}"
   end
 end
