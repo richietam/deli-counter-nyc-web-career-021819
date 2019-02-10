891a36792bfe4ff5d94a18f8a70226226f229f83
richardtam.ny <richardtam.ny@gmail.com>
2019-02-10 22:04:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty"
+  if katz_deli.length == 1
+    puts "Cur"
+  end
+end
