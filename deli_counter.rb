98589eea7af3c95670620dbbf112b8ac3ca39fb7
richardtam.ny <richardtam.ny@gmail.com>
2019-02-10 23:25:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,12 @@ def take_a_number(katz_deli, name)
     katz_deli.push(name)
     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is no one on line."
+  else
+    puts "Currently serving #{katz_deli[0]}"
+    katz_deli.pop()
+
+end
